1738ddd4d0a8f98e1871e40e75d80901594fca99
cristaor <cristaor@hotmail.com>
2023-09-27 21:38:37 -0500

    Iniciando IOT MOn

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#IP de la instancia donde corre el viewer
-ALLOWED_HOSTS = ["localhost", "54.161.111.216"]
+ALLOWED_HOSTS = ["localhost", "35.173.126.123"]
 
 
 # Application definition
@@ -43,7 +42,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'control',
     'receiver',
-    'viewer'
+    'viewer',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.82.209.39",  # Dirección IP de la base de datos
+        "HOST": "34.203.189.18",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "52.207.233.87"
+MQTT_HOST = "44.204.146.133"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
